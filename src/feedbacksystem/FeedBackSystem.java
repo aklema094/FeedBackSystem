dc29f008eef3d5f8be1e99e8b7db7f3fdaeaee96
aklema094 <aklemasultana094@gmail.com>
2025-01-10 21:46:50 +0600

    added user menu

--- src/feedbacksystem/FeedBackSystem.java
+++ src/feedbacksystem/FeedBackSystem.java
@@ -12,13 +12,38 @@ public class FeedBackSystem {
     
     
     
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException{
         
         Scanner sc = new Scanner(System.in);
         
         Class.forName("com.mysql.cj.jdbc.Driver");
         try{
             Connection con = DriverManager.getConnection(url,userName,password); 
+            
+            while(true){
+                System.out.println("WELCOME TO FEEDBACK SYSTEM");
+                System.out.println("==========================");
+                System.out.println("1. LogIn");
+                System.out.println("2. Sign Up");
+                System.out.println("3. Exit");
+                System.out.print("Enter your choice : ");
+                int ch = sc.nextInt();
+                switch(ch){
+                    case 1: 
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        exit();
+                        return;
+                    default :
+                        System.out.println("");
+                        System.out.println("Invalid Choice!!!!");
+                        break;
+                }
+                
+                
+            }
                
         }catch(SQLException e){
             e.printStackTrace();     
@@ -26,6 +51,15 @@ public class FeedBackSystem {
         
         
       
+    }
+    
+    public static void exit() throws InterruptedException{
+        System.out.print("Existing System");
+        for (int i = 0; i < 7; i++) {
+            System.out.print(". ");
+            Thread.sleep(250);   
+        }
+        System.out.println("");
     }
     
 }
